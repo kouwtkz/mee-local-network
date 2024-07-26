ababca206dca648faa6a838a1cac42f65d63a192
kouwtkz <kouwtkz@cottonwind.com>
2024-07-27 07:40:48 +0900

    Twitterログ用のタイプ定義ファイルの作成

---
+++ src/types/TwitterType.d.ts
@@ -0,0 +1,56 @@
+interface DMMessageRawType {
+  id: string;
+  senderId: string;
+  recipientId: string;
+  createdAt: string;
+  text: string;
+  urls: string[];
+  mediaUrls: string[];
+  reactions?: [
+    {
+      senderId: string;
+      reactionKey: string;
+      eventId: string;
+      createdAt: string;
+    }
+  ];
+}
+
+interface DMMessageType extends DMMessageRawType {
+  date: Date;
+  conversationId: string;
+}
+interface UserRawType {
+  username: string;
+  accountId: string;
+  createdAt?: string;
+  accountDisplayName: string;
+  img?: string;
+  re?: string | RegExp;
+  listIn?: true;
+  link?: string;
+  enableLink?: true;
+  index?: number;
+}
+
+interface UserType extends UserRawType {
+  date?: Date;
+}
+
+type DMMessagesRawPartType = KeyValueType<DMMessagesRawType>;
+type DMMessagesRawType = {
+  dmConversation: {
+    conversationId: string;
+    messages: [
+      {
+        messageCreate: DMMessageRawType;
+      }
+    ];
+  };
+}[];
+
+declare const YTD: {
+  user?: {
+    regist?: KeyValueType<UserRawType>;
+  };
+};
