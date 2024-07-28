4eaa8ef6e9305eaacb0d88a5112db2b6b13ba77f
kouwtkz <kouwtkz@cottonwind.com>
2024-07-29 06:59:10 +0900

    デプロイ用のスクリプトを見直した

--- deploy_rs.ps1
+++ deploy_rs.ps1
@@ -1,8 +1,9 @@
 "Upload to Raspberry pi Server…"
 (Measure-Command {
-    ubuntu run ("rsync -au -e ssh" + " rs:~/mee-local-network/data/ ./data/")
-    ubuntu run ("rsync -au -e ssh --delete" + " rs:~/mee-local-network/static/images/uploads/ ./static/images/uploads/")
-    # ubuntu run ("rsync -au -e ssh --delete" + " ./static/ rs:~/mee-local-network/static/")
-    ubuntu run ("rsync -au -e ssh --delete" + " ./dist/ rs:~/mee-local-network/dist/")
-    ubuntu run ("rsync -au -e ssh --delete" + " --exclude='node_modules/' --exclude='data/' --exclude='static/images/uploads/'}" + " ./ rs:~/mee-local-network/")
+    ubuntu run ("rsync -e ssh -au" + " rs:~/mee-local-network/data/ ./data/")
+    ubuntu run ("rsync -e ssh -au --delete" + " rs:~/mee-local-network/static/images/uploads/ ./static/images/uploads/")
+    ubuntu run ("rsync -e ssh -au --delete" + " ./dist/ rs:~/mee-local-network/dist/")
+    ubuntu run ("rsync -e ssh -au --delete" + " ./data/ rs:~/mee-local-network/data/")
+    # ubuntu run ("rsync -e ssh -au --delete" + " ./static/ rs:~/mee-local-network/static/")
+    ubuntu run ("rsync -e ssh --update --files-from=deploy_rsync_files.txt ./ rs:~/mee-local-network/")
 }).TotalSeconds.ToString('0.0') + 's'
