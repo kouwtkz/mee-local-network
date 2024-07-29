9a7cbdf9679ed06232847834212b2604781f13d7
kouwtkz <kouwtkz@cottonwind.com>
2024-07-29 12:25:32 +0900

    update deploy_rs.ps1

--- deploy_rs.ps1
+++ deploy_rs.ps1
@@ -4,6 +4,6 @@
     ubuntu run ("rsync -e ssh -au --delete" + " rs:~/mee-local-network/static/images/uploads/ ./static/images/uploads/")
     ubuntu run ("rsync -e ssh -au --delete" + " ./dist/ rs:~/mee-local-network/dist/")
     ubuntu run ("rsync -e ssh -au --delete" + " ./data/ rs:~/mee-local-network/data/")
-    # ubuntu run ("rsync -e ssh -au --delete" + " ./static/ rs:~/mee-local-network/static/")
+    ubuntu run ("rsync -e ssh -au --delete" + " ./static/ rs:~/mee-local-network/static/")
     ubuntu run ("rsync -e ssh --update --files-from=deploy_rsync_files.txt ./ rs:~/mee-local-network/")
 }).TotalSeconds.ToString('0.0') + 's'
