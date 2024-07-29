896397210e30764d1bf027f0389dae7b8b9032ad
kouwtkz <kouwtkz@cottonwind.com>
2024-07-29 12:26:37 +0900

    開発限定でインポートしたファイル取得用のパスを設定した

--- src/dev.tsx
+++ src/dev.tsx
@@ -3,11 +3,13 @@ import { CompactCode } from "./functions/StrFunctions";
 import importStyles from "./styles.scss?inline";
 import { ServerCommon } from "./server";
 import ssgApp from "./ssg";
+import { serveStatic } from "@hono/node-server/serve-static";
 
 const app = new Hono<MeeBindings>({ strict: false });
 const compactStyles = CompactCode(importStyles as string);
 app.get("/assets/styles.css", (c) => c.body(compactStyles));
 app.route("/", ssgApp);
+app.get("*", serveStatic({ root: "/import/result" }));
 
 ServerCommon(app);
 
