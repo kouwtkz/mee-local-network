b9716423923bb086d88a4bfed3dcf5939d684d34
kouwtkz <kouwtkz@cottonwind.com>
2024-08-21 15:06:36 +0900

    ラズパイからデータを引っ張るコマンドで古いファイルを飛ばさないようにした

--- importFromRs.bash
+++ importFromRs.bash
@@ -1,2 +1,2 @@
-rsync -v -e ssh -au rs:~/mee-local-network/data/ ./data/
-rsync -v -e ssh -au --delete rs:~/mee-local-network/static/images/uploads/ ./static/images/uploads/
+rsync -v -e ssh -a rs:~/mee-local-network/data/ ./data/
+rsync -v -e ssh -a --delete rs:~/mee-local-network/static/images/uploads/ ./static/images/uploads/
