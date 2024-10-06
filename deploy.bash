d91e2e8dbe23443b19f691a4fed16cf77bba08a8
kouwtkz <kouwtkz@cottonwind.com>
2024-10-06 11:45:46 +0900

    ローカルからリモートへデータベースの上書きをしないことにした

--- deploy.bash
+++ deploy.bash
@@ -8,7 +8,7 @@ SECONDS=0
 rsync -v -e ssh -au rs:~/mee-local-network/data/ ./data/
 rsync -v -e ssh -au --delete rs:~/mee-local-network/static/images/uploads/ ./static/images/uploads/
 rsync -v -e ssh -au --delete ./dist/ rs:~/mee-local-network/dist/
-rsync -v -e ssh -au --delete ./data/ rs:~/mee-local-network/data/
+# rsync -v -e ssh -au --delete ./data/ rs:~/mee-local-network/data/
 rsync -v -e ssh -au --delete ./static/ rs:~/mee-local-network/static/
 rsync -v -e ssh --update --files-from=deploy_rsync_files.txt ./ rs:~/mee-local-network/
 
